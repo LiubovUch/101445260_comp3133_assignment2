c8ed106943a40b10c67ca4f2ffea767e02782582
Liubov Uchamprina <156096417+LiubovUch@users.noreply.github.com>
2025-04-06 03:02:55 -0400

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://101445260comp3133assignment2-production.up.railway.app/'
+  apiUrl: 'https://101445260comp3133assignment2-production.up.railway.app/graphql'
 };
